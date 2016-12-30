b81198139f501bbe7dadfcdbe0ff336a61cd70f2
Ben Pepper <bfpepper87@gmail.com>
2016-12-30 11:44:26 -0700

    Add database cleaner.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,3 +39,18 @@ RSpec.configure do |config|
     end
   end
 end
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+end
