43af1dee21412f669dbe306eaa4624eaa2f08a06
Ben Pepper <bfpepper87@gmail.com>
2017-01-03 08:26:14 -0700

    user can log out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.destroy
+    flash[:success] = "You Successfully Logged out!"
+    redirect_to root_path
+  end
+
 end
--- spec/features/users/user_can_log_out_spec.rb
+++ spec/features/users/user_can_log_out_spec.rb
@@ -4,10 +4,16 @@ describe "User can log out" do
   scenario "A logged in user can log out" do
     user = create(:user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    
     visit root_path
 
+    click_on "Log In"
+
+    fill_in "session[email]", with: user.email
+    fill_in "session[password]", with: user.password
+
+    click_on "Login"
+
+    visit user_path(user)
     expect(page).to have_content("Log Out")
 
     click_on "Log Out"
